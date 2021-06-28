b19ccf43440fc8af887159508acb564161f818dd
filius-fall <sreeram596@gmail.com>
2021-06-28 14:13:27 +0530

    Complete API

--- api_app/views.py
+++ api_app/views.py
@@ -1,10 +1,9 @@
 from flask import Flask,request,jsonify,render_template,g
-
+import requests
 from .api_data import api_json_values,external_data
 from .db import get_db
 
 import json
-import ast
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -21,28 +20,8 @@ def index():
 
     return render_template('index.html',api_data=data)
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
 
-@app.route('/api/v1/data/all',methods=['GET'])
-def api_data_all():
-    return jsonify(api_json_values)
 
-@app.route('/api/v1/data/',methods=['GET'])
-def api_data_id():
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return 'Error: No id found'
-    
-    values = []
-
-    for data in api_json_values:
-        if id == data['id']:
-            values.append(data)
-
-    return jsonify(values)
 
 
 @app.errorhandler(404)
@@ -52,22 +31,31 @@ def page_not_found(e):
 
 @app.route('/api/v2/data/all',methods=['GET'])
 def api_all():
-    k = g.db.hgetall('user:test')
+    k = get_data()
     return jsonify(k)
 
 
 
-@app.route('/api/v2/external',methods=['GET',"POST"])
+@app.route('/api/v2/til_page',methods=["POST"])
 def external_data_post():
     if 'password'  not in request.args or request.args['password'] != 'password':
         return "You are not authorised"
-    if 'data' in request.args:
-        data = ast.literal_eval(request.args['data'])
+
+    request_data = request.get_json()
+
     
-    titles = ['title','url','descryption']
+    title = request_data['title']
+    url = request_data['url']
+    descryption = request_data['descryption']
+
+
+    
     user_id = str(g.db.incrby('next_user_id',1000))
-    for i in titles:
-        g.db.hset('user:' + user_id,i,data[i])
+
+
+    g.db.hset('user:' + user_id,'title',title)
+    g.db.hset('user:' + user_id,'url',url)
+    g.db.hset('user:' + user_id,'descryption',descryption)
 
     
     g.db.lpush('users',user_id)
@@ -76,11 +64,10 @@ def external_data_post():
         
 
 
-    return jsonify(data)
+    return jsonify([title,url,descryption])
 
 def get_data():
     
-    # titles = ['title','url','descryption']
     user_list = g.db.lrange('users',0,-1)
     print(user_list)
     data_list = []
@@ -90,3 +77,11 @@ def get_data():
         data_list.append(data)
 
     return data_list
+
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
